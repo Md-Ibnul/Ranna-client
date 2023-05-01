0148c033cb1394d4261ef3190148da5c1c9e8e0a
Md-Ibnul <ibnulhasan456@gmail.com>
2023-05-01 23:38:32 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnYlPGrsrm0EXesIRWERCT_xFsCPby0RQ",
+  authDomain: "ranna-e1bfa.firebaseapp.com",
+  projectId: "ranna-e1bfa",
+  storageBucket: "ranna-e1bfa.appspot.com",
+  messagingSenderId: "852408280405",
+  appId: "1:852408280405:web:cd4bd36359387faed7bbdc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavRanna from '../../layouts/NavRanna';
+import Banner from '../../layouts/banner/Banner';
+
+const Home = () => {
+    return (
+        <>
+            <NavRanna />
+            <Banner />
+        </>
+    );
+};
+
+export default Home;
